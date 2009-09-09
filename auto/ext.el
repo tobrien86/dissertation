e82c69664a1acc915d48f2c0d7b406fafddfe93c
Tom <thomas.obrien08@imperial.ac.uk>
2009-09-09 20:18:36 +0100

    almost done

--- auto/ext.el
+++ auto/ext.el
@@ -1,5 +1,9 @@
 (TeX-add-style-hook "ext"
  (lambda ()
     (LaTeX-add-labels
-     "sec:abel-extens-Qi")))
+     "sec:abel-extens-Qi"
+     "eq:3-torsion-polynomial"
+     "eq:beta-powers-1"
+     "eq:beta-powers-2"
+     "eq:min-poly-beta")))
 
